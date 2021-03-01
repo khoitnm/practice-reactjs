2a9b1e1d54380852a4cc14f7998da4883c270bb4
trank <Kevin.Tran@pointclickcare.com>
2021-03-01 15:54:51 -0500

    Rename `pro-00-1-react-ts-env-vars`

--- pro-00-1-react-ts-cross-env/build-tools/EnvLoader.ts
+++
@@ -1,9 +0,0 @@
-// import { dotenv } from 'dotenv';
-const dotenv = require('dotenv');
-const result = dotenv.config()
-
-console.log(`Loaded env variables when building app: ${JSON.stringify(process.env, null, 2)}`)
-
-if (result.error) {
-    console.error('Error when loading dotenv. Cause:', result.error);
-}

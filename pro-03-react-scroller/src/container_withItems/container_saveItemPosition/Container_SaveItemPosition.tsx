deb41c6edc64ba161ac9b95fb6303d8fd4dfbfc3
trank <Kevin.Tran@pointclickcare.com>
2023-03-01 11:33:57 -0500

    pro-03-react-scroller: Successfully return back to the selected item.

--- pro-03-react-scroller/src/container_withItems/container_saveItemPosition/Container_SaveItemPosition.tsx
+++ pro-03-react-scroller/src/container_withItems/container_saveItemPosition/Container_SaveItemPosition.tsx
@@ -7,17 +7,25 @@ const Container_SaveItemPosition = (): JSX.Element => {
   const [items, setItems] = useState<Item[]>([]);
   const [visibility, setVisibility] = useState<boolean>(true);
   const [scrollPosition, setScrollPosition] = useState<number>(0);
-  const scrollRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  const containerRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  const itemsRef = useRef<Array<HTMLDivElement | null>>([])
   const [selectedItemId, setSelectedItemId] = useState<string | undefined>();
-
+  // const [selectedItemPosition, setSelectedItemPosition] = useState<number | undefined>();
 
   useEffect(() => {
-    console.log(`ScrollRef.current is changed (ScrollRef itself doesn't change): ${scrollRef?.current}`);
-    if (scrollRef?.current) {
-      console.log(`ScrollRef scrollTo ${scrollPosition}`);
-      scrollRef.current!.scrollTo({top: scrollPosition});
+    console.log(`containerRef.current is changed (ScrollRef itself doesn't change): ${containerRef?.current}`);
+    if (containerRef?.current) {
+      if (selectedItemId) {
+        console.log(`containerRef scrollTo itemId ${selectedItemId}`);
+        const itemPosition = findPositionOfItem(selectedItemId);
+        console.log(`position of itemId ${itemPosition}`);
+        containerRef.current!.scrollTo({top: itemPosition});
+      } else {
+        console.log(`containerRef scrollTo previous position: ${scrollPosition}`);
+        containerRef.current!.scrollTo({top: scrollPosition});
+      }
     }
-  }, [scrollRef?.current])
+  }, [containerRef.current, itemsRef.current])
 
   const onClickRegenItems = () => {
     setItems(oldState => createItemsSortedRandomly(50));
@@ -38,8 +46,8 @@ const Container_SaveItemPosition = (): JSX.Element => {
   }
 
   const onScroll = () => {
-    console.log(`Save scroll position into React state: ${scrollRef?.current?.scrollTop}`)
-    setScrollPosition(scrollRef?.current?.scrollTop);
+    console.log(`Save scroll position into React state: ${containerRef?.current?.scrollTop}`)
+    setScrollPosition(containerRef?.current?.scrollTop);
   }
 
   const onClickChangeVisibilityWithSameScrollPosition = () => {
@@ -47,12 +55,44 @@ const Container_SaveItemPosition = (): JSX.Element => {
     setVisibility(oldState => !oldState);
   }
 
-  const onClickItem = (itemId: string) => {
-    setSelectedItemId(itemId);
+  const onClickItem = (item: Item, index: number) => {
+    setSelectedItemId(item.id);
   }
 
-  const listItemsRender = items.map((item) =>
-    <div className="item" key={item.id} onClick={() => onClickItem(item.id)}>id: {item.id}, name: {item.name}</div>
+  const findPositionOfItem = (itemId: string): number | undefined => {
+    const itemRef = findItemRef(itemId);
+    return getRelativeItemPosition(itemRef);
+  }
+
+  const findItemRef = (itemId: string): HTMLElement | null => {
+    for (let i = 0; i < itemsRef.current.length; i++) {
+      const itemRef = itemsRef.current[i];
+      const itemKey = itemRef?.getAttribute("itemid");
+      if (itemKey && itemKey == itemId) {
+        console.log(`Found itemKey: ${itemKey} at index ${i}`);
+        return itemRef;
+      }
+    }
+    return null;
+  }
+
+  const getRelativeItemPosition = (itemRef: HTMLElement | null): number|undefined => {
+    const absItemPosition = itemRef?.offsetTop;
+    const absContainerPosition = containerRef.current?.offsetTop;
+    const relativeItemPosition = absItemPosition && absContainerPosition ? absItemPosition - absContainerPosition : undefined;
+    return relativeItemPosition;
+  }
+
+  const listItemsRender = items.map((item, index) =>
+    <div
+      key={item.id}
+      itemID={item.id}
+      ref={el => itemsRef.current[index] = el}
+      onClick={() => onClickItem(item, index)}
+      className="item"
+    >
+      [{index}], id: {item.id}, name: {item.name}
+    </div>
   );
 
   return (
@@ -72,7 +112,7 @@ const Container_SaveItemPosition = (): JSX.Element => {
       <div>Scroll Position: {scrollPosition}</div>
       <div>Selected Item Id: {selectedItemId}</div>
       {visibility &&
-      <div className="container" ref={scrollRef} onScroll={onScroll}>
+      <div className="container" ref={containerRef} onScroll={onScroll}>
           <div>{listItemsRender}</div>
       </div>
       }

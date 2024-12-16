b6480052ab9ca3487915fd877051aa5a969c538c
Kevin Tran <Kevin.Tran@pointclickcare.com>
2024-12-16 15:11:54 -0500

    Log JSON avoid circular reference.

--- pro-05-react-richtextarea/src/App.tsx
+++ pro-05-react-richtextarea/src/App.tsx
@@ -1,29 +1,77 @@
-import React from 'react';
-
-const showNotification = () => {
-    if ("Notification" in window) {
-        Notification.requestPermission().then((permission) => {
-            if (permission === "granted") {
-                const notification = new Notification("Notification Title", {
-                    body: "Notification Body",
-                });
-
-                notification.addEventListener("click", () => {
-                    window.focus(); // it will open the browser tab that has the web app that showing the notification.
-                    notification.close();
-                });
-            }
-        });
+import React, {useCallback, useState} from 'react';
+import {createEditor, Editor, Transforms, Element, Descendant, BaseEditor} from 'slate';
+import {Editable, ReactEditor, Slate, withReact} from 'slate-react';
+import {getCircularReplacer} from "./LogHelper";
+// TYPES ////////////////////////////////////
+declare module 'slate' {
+    interface CustomTypes {
+        Editor: BaseEditor & ReactEditor
+        Element: CustomElement
+        Text: CustomText
     }
-};
+}
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+// TYPES ////////////////////////////////////
+
+// Define a React component renderer for our code blocks.
+const CodeElement = (props: any) => {
+    return (
+        <pre {...props.attributes}>
+            <code>{props.children}</code>
+        </pre>
+    )
+}
+const DefaultElement = (props: any) => {
+    return <p {...props.attributes}>{props.children}</p>
+}
 
 function App() {
+    const [editor] = useState(() => withReact(createEditor()));
+    const initialValue: CustomElement[] = [
+        {
+            type: 'paragraph',
+            children: [{ text: 'A line of text in a paragraph.' }],
+        },
+    ];
+
+    // Define a rendering function based on the element passed to `props`. We use
+    // `useCallback` here to memoize the function for subsequent renders.
+    const renderElement = useCallback((props: any) => {
+        console.log('props:', JSON.stringify(props, getCircularReplacer(), 2));
+        switch (props.element.type) {
+            case 'code':
+                return <CodeElement {...props} />
+            default:
+                return <DefaultElement {...props} />
+        }
+    }, []);
     return (
-        <div className="App">
-            <header className="App-header">
-                <button onClick={showNotification}>Show Notification</button>
-            </header>
-        </div>
+        <Slate editor={editor} initialValue={initialValue}>
+            <Editable
+                // Pass in the `renderElement` function.
+                renderElement={renderElement}
+                onKeyDown={event => {
+                    if (event.key === '&') {
+                        event.preventDefault()
+                        editor.insertText('and')
+                    }
+
+                    if (event.key === '`' && event.ctrlKey) {
+                        // Prevent the "`" from being inserted by default.
+                        event.preventDefault()
+                        // Otherwise, set the currently selected blocks type to "code".
+                        Transforms.setNodes(
+                            editor,
+                            { type: 'code' },
+                            { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
+                        )
+                    }
+                }}
+            />
+        </Slate>
     );
 }
 
---
+++ pro-05-react-richtextarea/src/LogHelper.ts
@@ -0,0 +1,12 @@
+export const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key: any, value: any) => {
+        if (typeof value === "object" && value !== null) {
+            if (seen.has(value)) {
+                return "[Circular]";
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
